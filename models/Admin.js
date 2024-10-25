c48489fed95cd2fb17931ee5773ddb4fbf775b7b
msvicky29 <vickyms2983@gmail.com>
2024-10-25 23:50:00 +0530

    First commit

---
+++ models/Admin.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const adminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Hash password before saving
+adminSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare password - fixed to return a Promise
+adminSchema.methods.comparePassword = function(candidatePassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+            if (err) return reject(err);
+            resolve(isMatch);
+        });
+    });
+};
+
+module.exports = mongoose.model('Admin', adminSchema);
